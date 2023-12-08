719eb8099c4e74714853dc435d7e38282017b6ca
Max Winokan <mwinokan@me.com>
2023-12-08 12:04:07 +0000

    hippo2

--- hippo2/plotting.py
+++ hippo2/plotting.py
@@ -2,7 +2,16 @@
 import plotly.express as px
 import pandas as pd
 
-def plot_tag_statistics(animal):
+'''
+
+	ALL GRAPHS DEFINED HERE SHOULD:
+
+	* Have a HIPPO logo
+	* Include the target name in the title
+
+'''
+
+def plot_tag_statistics(animal, color=None, subtitle=None):
 
 	plot_data = []
 
@@ -14,11 +23,20 @@ def plot_tag_statistics(animal):
 
 		plot_data.append(data)
 
-	fig = px.bar(plot_data, x='tag', y='num_poses', title='Number of poses per tag')
+	fig = px.bar(plot_data, x='tag', y='num_poses', color=None)
+
+	title = 'Tag Statistics'
+
+	if subtitle:
+		title = f'<b>{animal.target_name}</b>: {title}<br><sup><i>{subtitle}</i></sup>'
+	else:
+		title = f'<b>{animal.target_name}</b>: {title}'
+	
+	fig.update_layout(title=title,title_automargin=False, title_yref='container')
 
 	fig.update_layout(xaxis_title='Tag', yaxis_title='#Poses')
 
-	return fig
+	return add_hippo_logo(fig)
 
 def plot_interaction_histogram(animal, poses, feature_metadata, subtitle=None,):
 
@@ -53,12 +71,16 @@ def plot_interaction_histogram(animal, poses, feature_metadata, subtitle=None,):
 	title='Leveraged protein features'
 
 	if subtitle:
-		fig.update_layout(title=f'{title} ({subtitle})')
+		title = f'<b>{animal.target_name}</b>: {title}<br><sup><i>{subtitle}</i></sup>'
+	else:
+		title = f'<b>{animal.target_name}</b>: {title}'
+	
+	fig.update_layout(title=title,title_automargin=False, title_yref='container')
 
 	fig.update_layout(xaxis_title='Residue')
 	fig.update_layout(yaxis_title='#Interactions')
 
-	return fig
+	return add_hippo_logo(fig)
 
 def plot_interaction_punchcard(animal, poses, feature_metadata, subtitle=None, opacity=1.0):
 
@@ -90,11 +112,18 @@ def plot_interaction_punchcard(animal, poses, feature_metadata, subtitle=None, o
 
 	plot_df = pd.DataFrame(plot_data)
 
-	title='Interaction Punch-Card'
+	title = 'Interaction Punch-Card'
+
 	if subtitle:
-		title = f'{title}: {subtitle}'
+		title = f'<b>{animal.target_name}</b>: {title}<br><sup><i>{subtitle}</i></sup>'
+	else:
+		title = f'<b>{animal.target_name}</b>: {title}'
 
 	fig = px.scatter(plot_df, x='res_name_number_chain_str', y='family',marginal_x='histogram',marginal_y='histogram', hover_data=plot_df.columns, color='pose', title=title)
+	
+	fig.update_layout(title=title,title_automargin=False, title_yref='container')
+
+	fig.update_layout(xaxis_title='Residue', yaxis_title='Feature Family')
 
 	for trace in fig.data:
 		if type(trace) == plotly.graph_objs._histogram.Histogram:
@@ -107,15 +136,66 @@ def plot_interaction_punchcard(animal, poses, feature_metadata, subtitle=None, o
 	fig.update_layout(barmode='stack')
 	fig.update_layout(scattermode='group', scattergap=0.75)
 
+	# return add_hippo_logo(fig, in_plot=False)
+	return add_punchcard_logo(fig)
+
+HIPPO_LOGO_URL = 'https://raw.githubusercontent.com/mwinokan/HIPPO/main/logos/hippo_logo_tightcrop.png'
+HIPPO_HEAD_URL = 'https://raw.githubusercontent.com/mwinokan/HIPPO/main/logos/hippo_assets-02.png'
+
+def add_hippo_logo(fig, in_plot=True):
+
+	assert fig.layout.title.text, 'Figure must have a title to add the HIPPO logo'
+
+	if in_plot:
+
+		fig.add_layout_image(dict(
+			source=HIPPO_LOGO_URL,
+			xref="paper", yref="paper",
+			# layer='below',
+			
+			x=0.95, y=0.95,
+			sizex=0.3, sizey=0.3,
+			xanchor="right", yanchor="top",
+		))
+
+		return fig
+
+	has_legend = fig.layout.legend.title.text is not None
+	fig.layout.margin.t = None
+
+	if has_legend:
+
+		fig.add_layout_image(dict(
+			source="",
+			xref="paper", yref="paper",
+			
+			x=1, y=1.05,
+			sizex=0.4, sizey=0.4,
+			xanchor="left", yanchor="bottom",
+		))
+
+	else:
+
+		fig.add_layout_image(dict(
+			source=HIPPO_LOGO_URL,
+			xref="paper", yref="paper",
+			
+			x=1, y=1.05,
+			sizex=0.3, sizey=0.3,
+			xanchor="right", yanchor="bottom",
+		))
+	
 	return fig
 
-def add_hippo_logo(fig):
+def add_punchcard_logo(fig):
+
 	fig.add_layout_image(dict(
-			source="https://raw.githubusercontent.com/cldougl/plot_images/add_r_img/vox.png",
-			xref="paper", yref="paper",
-			x=1, y=1.05,
-			sizex=0.2, sizey=0.2,
-			xanchor="right", yanchor="bottom"
+		source=HIPPO_HEAD_URL,
+		xref="paper", yref="paper",
+		
+		x=1, y=1,
+		sizex=0.25, sizey=0.25,
+		xanchor="right", yanchor="top",
 	))
-	
+
 	return fig
