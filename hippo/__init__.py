0329136ea7d0573021aac93118944ff46ba3621c
Max Winokan <mwinokan@me.com>
2024-06-13 09:06:18 +0000

    RandomRecipeGenerator and Recipe at package root

--- hippo/__init__.py
+++ hippo/__init__.py
@@ -1,7 +1,9 @@
 
-__version__ = '0.3.5'
+__version__ = '0.3.21'
 
 from .animal import HIPPO
 from .compound import Compound
 from .pycule import Quoter
+from .rgen import RandomRecipeGenerator
+from .recipe import Recipe
 # from .scoring import Scorer, CustomAttribute
