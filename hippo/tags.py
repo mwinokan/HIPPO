0bc105daa4e5d59fe2bc81188c8bf6d084ed2286
Max Winokan <mwinokan@me.com>
2024-08-23 11:44:08 +0100

    tags and target docstrings

--- hippo/tags.py
+++ hippo/tags.py
@@ -4,33 +4,40 @@ from collections.abc import MutableSet
 import mcol
 
 class TagTable:
-	"""Object representing the 'tag' table in the :class:`.Database`."""
+	
+	"""Object representing the 'tag' table in the :class:`.Database`.
+
+	.. attention::
+
+		:class:`.TagTable` objects should not be created directly. Instead use the :meth:`.HIPPO.tags` property.
+	
+	"""
+
+	_table = 'tag'
 
 	def __init__(self, 
-		db, 
-		table: str = 'tag',
+		db: 'Database', 
 	) -> None:
 		
 		self._db = db
-		self._table = table
 
 	### FACTORIES
 
 	### PROPERTIES
 
 	@property
-	def db(self):
-		"""Returns the associated :class:`.database`"""
+	def db(self) -> 'Database':
+		"""Returns a pointer to the parent database"""
 		return self._db
 	
 	@property
 	def table(self) -> str:
-		""" """
+		"""Returns the name of the :class:`.Database` table"""
 		return self._table
 
 	@property
-	def unique(self) -> set:
-		"""Returns a set of tag names contained in the table"""
+	def unique(self) -> set[str]:
+		"""Returns a set of unique tag names contained in the table"""
 		values = self.db.select(table=self.table, query='DISTINCT tag_name', multiple=True)
 		return set(v for v, in values)
 
@@ -38,29 +45,39 @@ class TagTable:
 
 	### DUNDERS
 
-	def __getitem__(self, key):
-		raise NotImplementedError
-
-	def __repr__(self):
+	def __repr__(self) -> str:
+		"""Formatted representation of this object"""
 		return f'{mcol.bold}{mcol.underline}Tags {self.unique}{mcol.clear}'
 
 
 class TagSet(MutableSet):
-	"""Object representing a subset of the 'tag' table in the :class:`.Database` belonging to a certain :class:`.Compound` or :class:`.Pose`."""
+	
+	"""Object representing a subset of the 'tag' table in the :class:`.Database` belonging to a certain :class:`.Compound` or :class:`.Pose`.
+
+	.. attention::
+
+		:class:`.TagSet` objects should not be created directly. Instead use the :meth:`.Compound.tags` or :meth:`.Pose.tags` property.
+
+	"""
 
 	def __init__(self, 
-		parent, # Compound or Pose
-		tags=(), 
-		immutable=False,
-		commit=True,
+		parent: 'Compound | Pose',
+		tags: list | tuple | None = None, 
+		immutable: bool = False,
+		commit: bool = True,
 	):
 		
 		self._elements = []
 		self._immutable = immutable
 		self._parent = parent
 
+		tags = tags or ()
+
 		for tag in tags:
-			self.add(tag, commit=commit)
+			self.add(tag, commit=False)
+		
+		if commit:
+			self.db.commit()
 		
 	### FACTORIES
 
@@ -72,17 +89,14 @@ class TagSet(MutableSet):
 		return self._elements
 
 	@property
-	def immutable(self):
-		"""Returns whether this set is immutable"""
+	def immutable(self) -> bool:
+		"""Is this set is immutable?"""
 		return self._immutable
 	
 	@immutable.setter
-	def immutable(self,b):
-		"""
-
-		:param b: 
-
-		"""
+	def immutable(self, 
+		b: bool,
+	) -> None:
 		self._immutable = b
 
 	@property
@@ -91,37 +105,46 @@ class TagSet(MutableSet):
 		return self._parent
 
 	@property
-	def db(self):
-		"""Returns the associated :class:`.database`"""
+	def db(self) -> 'Database':
+		"""Returns a pointer to the parent database"""
 		return self.parent.db
-	
 
 	
 	### DATABASE
 
-	def _remove_tag_from_db(self, tag):
-		"""
+	def _remove_tag_from_db(self, 
+		tag: str,
+	) -> None:
 
-		:param tag: 
+		"""Delete a specific tag assignment for the parent :class:`.Compound`/:class:`.Pose`
+
+		:param tag: tag to delete
 
 		"""
-		sql = f'DELETE FROM tag WHERE tag_name="{tag}" AND tag_{self.parent.table}={self.parent.id}'
+		sql = f'DELETE FROM tag WHERE tag_name="{tag}" AND tag_{self.parent.table} = {self.parent.id}'
 		self.db.execute(sql)
 
-	def _clear_tags_from_db(self, tag):
-		"""
+	def _clear_tags_from_db(self, 
+		tag: str,
+	) -> None:
 
-		:param tag: 
+		"""Delete all tag assignments for the parent :class:`.Compound`/:class:`.Pose`
+
+		:param tag: tag to delete
 
 		"""
-		sql = f'DELETE FROM tag WHERE tag_{self.parent.table}={self.parent.id}'
+		sql = f'DELETE FROM tag WHERE tag_{self.parent.table} = {self.parent.id}'
 		self.db.execute(sql)
 
-	def _add_tag_to_db(self, tag, commit=True):
-		"""
+	def _add_tag_to_db(self, 
+		tag: str, 
+		commit: bool = True,
+	) -> None:
 
-		:param tag: 
-		:param commit:  (Default value = True)
+		"""Assign a given tag to the parent
+
+		:param tag: tag to add
+		:param commit: commit the changes? (Default value = True)
 
 		"""
 		payload = { 'name':tag, self.parent.table:self.parent.id }
@@ -130,30 +153,39 @@ class TagSet(MutableSet):
 
 	### METHODS
 
-	def pop(self):
+	def pop(self) -> str:
 		"""Pop the last element"""
 		assert not self.immutable
 		return self._elements.pop()
 
-	def discard(self, tag):
+	def discard(self,
+		tag: str,
+	) -> None:
+
 		"""Discard an element
 
-		:param tag: 
+		:param tag: tag to discard
 
 		"""
-		return self.discard(tag)
+
+		self.discard(tag)
 
 	def clear(self):
 		"""Clear all tags"""
 		self._elements = []
 		self._clear_tags_from_db(self)
 
-	def remove(self, tag):
+	def remove(self, 
+		tag: str
+	) -> None:
+		
 		"""Remove an element
 
-		:param tag: 
+		:param tag: tag to remove
+		:raises ValueError: if tag is not in set 
 
 		"""
+
 		assert not self.immutable
 		if tag in self:
 			i = self._elements.index(tag)
@@ -162,47 +194,67 @@ class TagSet(MutableSet):
 		else:
 			raise ValueError(f'{tag} not in {self}')
 
-	def add(self, tag, commit=True):
-		"""Add an element
+	def add(self, 
+		tag: str, 
+		commit: bool = True,
+	) -> None:
 
-		:param tag: 
-		:param commit:  (Default value = True)
+		"""Add a tag to the set
+
+		:param tag: tag to add
+		:param commit: commit the change? (Default value = True)
 
 		"""
+
 		assert not self.immutable
 		if tag not in self._elements:
 			self._elements.append(tag)
 			self._add_tag_to_db(tag, commit=commit)
 
-	def glob(self, pattern):
+	def glob(self, 
+		pattern: str
+	) -> list[str]:
+		
+		"""Construct a list from tags in the set names that match a given UNIX-style pattern.
+
+		:param pattern: unix style pattern with shell-style wildcards
+		:returns: list of tags
+
 		"""
 
-		:param pattern: 
-
-		"""
 		import fnmatch
 		return fnmatch.filter(self.tags, pattern)
 
 	### DUNDERS
 
-	def __contains__(self, tag):
+	def __contains__(self, 
+		tag: str
+	) -> bool:
+		"""Is this tag in the set?"""
 		return tag in self.tags
 
-	def __repr__(self):
+	def __repr__(self) -> str:
+		"""Formatted representation of this set"""
 		return str(self._elements)
 
-	def __len__(self):
+	def __len__(self) -> int:
+		"""Number of tags in this set"""
 		return len(self._elements)
 
 	def __iter__(self):
+		"""Iterate through this set"""
 		return iter(self._elements)
 
-	def __iter__(self):
-		return iter(self.tags)
-
 	def __add__(self, other):
-		raise NotImplementedError
-		# return TagTable(self.tags+other)
+		"""
+		.. attention::
 
-	def __getitem__(self, key):
+			Adding sets together is not supported
+
+		"""
+		raise NotImplementedError
+
+	def __getitem__(self, key: int):
+		"""Get a specific element in the set by index"""
 		return self._elements[key]
+
--- hippo/target.py
+++ hippo/target.py
@@ -2,31 +2,42 @@
 import mcol
 
 class Target:
-	"""Object representing a protein target"""
+	
+	"""Object representing a protein target
 
-	def __init__(self, db, id, name):
+	.. attention::
+	
+		:class:`.Target` objects should not be created directly. Instead use :meth:`.HIPPO.register_target` or :meth:`.Pose.target`.
+	
+	"""
+
+	def __init__(self, 
+		db: 'Database', 
+		id: int, 
+		name: str,
+	) -> None:
 
 		self._db = db
 		self._id = id
 		self._name = name
 
 	@property
-	def db(self):
-		""" """
+	def db(self) -> 'Database':
+		"""Returns a pointer to the parent database"""
 		return self._db
 
 	@property
-	def id(self):
+	def id(self) -> int:
 		"""Returns the target's ID"""
 		return self._id
 
 	@property
-	def name(self):
+	def name(self) -> str:
 		"""Returns the target's name"""
 		return self._name
 
 	@property
-	def feature_ids(self):
+	def feature_ids(self) -> list[int]:
 		"""Returns the target's feature ID's"""
 		feature_ids = self.db.select_where(
 			query='feature_id', 
@@ -44,22 +55,28 @@ class Target:
 		return feature_ids
 
 	@property
-	def features(self):
+	def features(self) -> list['Feature']:
 		"""Returns the target's features"""
 		if feature_ids := self.feature_ids:
 			return [self.db.get_feature(id=i) for i in feature_ids]
 		return None
 
-	def __str__(self):
+	def __str__(self) -> str:
+		"""Unformatted string representation"""
 		return f'T{self.id}'
 
-	def __repr__(self):
+	def __repr__(self) -> str:
+		"""Formatted string representation"""
 		return f'{mcol.bold}{mcol.underline}{self} "{self.name}"{mcol.unbold}{mcol.ununderline}'
 
-	def calculate_features(self, protein):
+	def calculate_features(self, 
+		protein: 'mp.System'
+	) -> list['Feature']:
+
 		"""Calculate features from a protein system
 
-		:param protein: 
+		:param protein: `molparse.System` object, likely from :meth:`.Pose.protein_system`
+		:returns: a list of :class:`.Feature` objects
 
 		"""
 
