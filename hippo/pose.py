c9d1b1830652fbb39c152a0699c621999dd14c04
Max Winokan <mwinokan@me.com>
2024-09-24 08:41:13 +0100

    draw2d

--- hippo/pose.py
+++ hippo/pose.py
@@ -1068,6 +1068,15 @@ class Pose:
 
         draw_mols(mols)
 
+    def draw2d(
+        self,
+    ) -> None:
+        """Draw a 2D drawing of this pose"""
+        from rdkit.Chem import MolFromSmiles
+
+        mol = MolFromSmiles(self.smiles)
+        display(mol)
+
     def render(
         self,
         protein="cartoon",
