63a80b7a584475389f4140e9ca9aaf6be34972e2
Max Winokan <mwinokan@me.com>
2024-10-16 12:08:12 +0100

    get_compound_id_pose_ids_dict get_pose_id_interaction_ids_dict

--- hippo/db.py
+++ hippo/db.py
@@ -2405,6 +2405,28 @@ class Database:
         records = self.execute("SELECT route_id, route_product FROM route").fetchall()
         return {route_id: route_product for route_id, route_product in records}
 
+    def get_compound_id_pose_ids_dict(self, cset: "CompoundSet") -> dict[int, set]:
+        """Get a dictionary mapping :class:`.Compound` ID's to their associated :class:`.Pose` ID's"""
+        records = self.execute(f"SELECT pose_compound, pose_id FROM pose WHERE pose_compound IN {cset.str_ids}").fetchall()
+        
+        d = {}
+
+        for comp_id, pose_id in records:
+            d[comp_id] = d.get(comp_id, set())
+            d[comp_id].add(pose_id)
+        return d
+
+    def get_pose_id_interaction_ids_dict(self, pset: "PoseSet") -> dict[int, set]:
+        """Get a dictionary mapping :class:`.Pose` ID's to their associated :class:`.Interaction` ID's"""
+        records = self.execute(f"SELECT interaction_pose, interaction_id FROM interaction WHERE interaction_pose IN {pset.str_ids}").fetchall()
+        
+        d = {}
+
+        for pose_id, interaction_id in records:
+            d[pose_id] = d.get(pose_id, set())
+            d[pose_id].add(interaction_id)
+        return d
+
     def get_interaction(self, *, id: int, table: str = "interaction") -> "Interaction":
         """Fetch the :class:`.Interaction` object with given ID
 
