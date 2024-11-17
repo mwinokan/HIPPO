dd57ba70a768f9c102fc73b79133fd5b3e5464d7
Max Winokan <mwinokan@me.com>
2024-11-17 22:21:42 +0000

    custom Mol field ok?

--- hippo_django_orm/models.py
+++ hippo_django_orm/models.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from django.db import models
 from .expressions import MolFromSmiles, MolPatternBfpFromSmiles
+from .fields import MolField
 
 
 class TargetModel(models.Model):
@@ -20,13 +21,20 @@ class CompoundModel(models.Model):
         app_label = "hippo"
         abstract = True
 
+    # regular fields
+
     id = models.BigAutoField(primary_key=True)
     inchikey = models.CharField(max_length=27, unique=True)
     alias = models.CharField(max_length=60, blank=True, unique=True)
     smiles = models.CharField(max_length=90)
+    metadata = models.JSONField(default=dict(), blank=True)
+
+    # chemicalite functions as custom Expressions in generated fields
+
     mol = models.GeneratedField(
         expression=MolFromSmiles("smiles", "mol"),
-        output_field=models.BinaryField(blank=True),
+        output_field=MolField(blank=True),
+        # output_field=models.BinaryField(blank=True),
         db_persist=True,
     )
 
@@ -35,8 +43,6 @@ class CompoundModel(models.Model):
         output_field=models.BinaryField(blank=True),
         db_persist=True,
     )
-
-    metadata = models.JSONField(default=dict(), blank=True)
 
 
 class PoseModel(models.Model):
@@ -55,6 +61,7 @@ class PoseModel(models.Model):
     path = models.FilePathField(Path("/"), max_length=200)
     compound = models.ForeignKey("Compound", on_delete=models.CASCADE)
     target = models.ForeignKey("Target", on_delete=models.CASCADE)
+    # mol = MolField(blank=True)
     mol = models.BinaryField(blank=True)
     energy_score = models.FloatField(blank=True, null=True)
     distance_score = models.FloatField(blank=True, null=True)
