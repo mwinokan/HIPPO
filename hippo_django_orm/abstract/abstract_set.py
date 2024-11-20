66f0882235b327cb94d809c8d08c002e722c0449
Max Winokan <mwinokan@me.com>
2024-11-20 18:51:00 +0000

    comments

--- hippo_django_orm/abstract/abstract_set.py
+++ hippo_django_orm/abstract/abstract_set.py
@@ -7,3 +7,11 @@ class AbstractQuerySet(models.QuerySet, AbstractTable):
     def __init__(self, *args, **kwargs):
         models.QuerySet.__init__(self, *args, **kwargs)
         self._name = None
+
+    ### PROPERTIES
+
+    ### METHODS
+
+    ### DUNDERS
+
+    # __getitem__ handled by models.QuerySet
