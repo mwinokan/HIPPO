de56a08c533ab6fd451e2a6aba2e9d1bf0ae8dca
Max Winokan <mwinokan@me.com>
2024-11-20 18:52:11 +0000

    FeatureSet

--- hippo_django_orm/feature/feature.py
+++ hippo_django_orm/feature/feature.py
@@ -1,7 +1,10 @@
 from ..models import FeatureModel
+from .feature_set import FeatureSet
 
 
 class Feature(FeatureModel):
+
+    _objects = FeatureSet.as_manager()
 
     def __str__(self):
         return f"{self.target} -> F{self.id}"
---
+++ hippo_django_orm/feature/feature_set.py
@@ -0,0 +1,15 @@
+from ..abstract import AbstractQuerySet
+
+
+class FeatureSet(AbstractQuerySet):
+
+    _shorthand = "F"
+    _max_str_ids = 5
+
+    ### FACTORIES
+
+    ### PROPERTIES
+
+    ### METHODS
+
+    ### DUNDERS
