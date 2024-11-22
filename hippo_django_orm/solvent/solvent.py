bc3f3089c9008decffb26cd798252f7919946e0f
Max Winokan <mwinokan@me.com>
2024-11-22 15:50:25 +0000

    __str__

--- hippo_django_orm/solvent/solvent.py
+++ hippo_django_orm/solvent/solvent.py
@@ -5,3 +5,6 @@ from .solvent_set import SolventSet
 class Solvent(SolventModel):
 
     _objects = SolventSet.as_manager()
+
+    def __str__(self) -> str:
+        return f'Solvent_{self.id} "{self.name}"'
