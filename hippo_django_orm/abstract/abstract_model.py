07b33e99ca6de2e9ca8f01ea850801c84eeefc6e
Max Winokan <mwinokan@me.com>
2024-11-22 15:49:51 +0000

    AbstractModel.summary()

--- hippo_django_orm/abstract/abstract_model.py
+++ hippo_django_orm/abstract/abstract_model.py
@@ -100,13 +100,59 @@ class AbstractModel(models.Model):
     def shorthand(self) -> str | None:
         return self._shorthand
 
+    ### METHODS
+
+    def get_wrapped_field_names(self) -> list[str]:
+
+        fields = self._meta.get_fields()
+
+        names = []
+        for field in fields:
+            name = field.name
+            name = name.removeprefix("_")
+            if name in names:
+                names = [n for n in names if n != name]
+            names.append(name)
+
+        return names
+
+    def summary(self):
+
+        from rich.table import Table
+
+        fields = self.get_wrapped_field_names()
+
+        table = Table(title=self.__rich__())
+        table.add_column("field")
+        table.add_column("value")
+
+        for field in fields:
+            value = getattr(self, field)
+
+            if hasattr(value, "__rich__"):
+                s = value.__rich__()
+            else:
+                s = str(value)
+            table.add_row(field, s)
+
+        mrich.print(table)
+
     ### DUNDERS
 
     def __str__(self) -> str:
-        if s := self.shorthand:
-            return f"{self.shorthand}{self.id}"
+
+        if sh := self.shorthand:
+            s = f"{self.shorthand}{self.id}"
         else:
-            return f"{self.__name__}_{self.id}"
+            s = f"{self.__name__}_{self.id}"
+
+        if hasattr(self, "alias") and (alias := self.alias):
+            return f'{s} "{alias}"'
+
+        if hasattr(self, "name") and (name := self.name):
+            return f'{s} "{name}"'
+
+        return s
 
     def __repr__(self) -> str:
         """ANSI formatted string representation"""
@@ -115,3 +161,6 @@ class AbstractModel(models.Model):
     def __rich__(self) -> str:
         """Representation for mrich"""
         return f"[bold underline]{self}"
+
+    def __name__(self):
+        return self.__class__.__name__
