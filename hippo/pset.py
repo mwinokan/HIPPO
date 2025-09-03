35367b9e15bd86f3283a02ca1850f90f706ac054
Max Winokan <mwinokan@me.com>
2025-09-03 17:11:24 +0100

    flatten zip path hierarchy

--- hippo/pset.py
+++ hippo/pset.py
@@ -2112,7 +2112,7 @@ class PoseSet:
             # create the zip archive
             with ZipFile(str(zip_path.resolve()), "w") as z:
                 for path in zips:
-                    z.write(path.relative_to(Path(out_path).parent))
+                    z.write(path, arcname=path.name)
 
             mrich.writing(f"{out_key}_refs.zip")
 
