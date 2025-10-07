cf57b2e730f0a514378008c565cbf89302da7f23
Max Winokan <mwinokan@me.com>
2025-10-07 16:42:36 +0200

    RandomRecipeGenerator: fix directory not being created

--- hippo/rgen.py
+++ hippo/rgen.py
@@ -117,7 +117,7 @@ class RandomRecipeGenerator(RRGMixin):
         # Recipe I/O set up
         path = Path(f"{out_key}_recipes")
         mrich.writing(f"{path}/")
-        if not path.exists:
+        if not path.exists():
             path.mkdir()
         self._recipe_dir = path
 
