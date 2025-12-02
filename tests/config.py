63102f4281588bb1ef7f45f6e373f65755fb5446
Max Winokan <mwinokan@me.com>
2025-12-02 11:29:47 +0000

    variable configuration

--- tests/config.py
+++ tests/config.py
@@ -3,14 +3,32 @@ TARGET = "SARS2_Nprot"
 PROPOSAL = "lb32627-93"
 STACK = "production"
 
+## CONFIGURE CLEANUP
+
+CLEANUP_FILES = [
+    # f"{TARGET}.tar.gz",
+]
+
+CLEANUP_DIRS = [
+    # TARGET,
+]
+
+## CONFIGURE DATABASE
+
+### SQLITE
+
+DB = "db_test.sqlite"
+
+CLEANUP_FILES.append(DB)
+
+### POSTGRES
+
 DB = dict(
     username="postgres",
     password="hippo",
     host="localhost",
     port=5432,
 )
-
-TARGET = "SARS2_Nprot"
 
 ## DISABLE TESTS
 
@@ -20,14 +38,3 @@ SETUP = True
 ADD_HITS = True
 SCAFFOLDS = True
 SUBSITES = True
-
-## CONFIGURE CLEANUP
-
-CLEANUP_FILES = [
-    # DB,
-    # f"{TARGET}.tar.gz",
-]
-
-CLEANUP_DIRS = [
-    # TARGET,
-]
