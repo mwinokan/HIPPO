bcdcb80dd9770f5091060d4b4c57cb07883bc23d
Kalev Takkis <ktakkis@informaticsmatters.com>
2026-05-22 15:29:30 +0100

    stashing

--- hippo/designdb/services/ingestion.py
+++ hippo/designdb/services/ingestion.py
@@ -7,10 +7,10 @@ from typing import Any
 import molparse as mp
 import mrich
 import pandas as pd
-from designdb.chem import InvalidChemistryError, UnsupportedChemistryError, check_chemistry
 from designdb.components.compound import Ingredient
 from designdb.components.recipe import Recipe, Route
 from designdb.models import (
+    ComponentModel,
     CompoundModel,
     PoseModel,
     ReactantModel,
@@ -31,6 +31,7 @@ from designdb.utils import (
     remove_other_ligands,
     sanitise_smiles,
 )
+from designdb.utils_chem import InvalidChemistryError, UnsupportedChemistryError, check_chemistry
 from designdb.utils_frag import UnsupportedFragalysisLongcodeError, parse_observation_longcode
 from django.db import connection
 from numpy import isnan
@@ -306,6 +307,8 @@ class IngestionService:
         skip_records: list[str],
         compound_tag_list: list[str],
         metadata_file: Path | str,
+        check_rmsd: bool = False,
+        rmsd_threshold: float = 1.0,
     ) -> IngestionBatchResult:
 
         # this is now strictly for loading frag data. cannot switch inner funcs easily
@@ -368,6 +371,8 @@ class IngestionService:
                 metadata=metadata,
                 inchikey=inchikey,
                 smiles=smiles,
+                check_rmsd=check_rmsd,
+                rmsd_threshold=rmsd_threshold,
             )
             if pose_created:
                 result.poses_created += 1
@@ -407,6 +412,8 @@ class IngestionService:
         skip_not_equal,
         convert_floats: bool = True,
         field_warning=None,
+        check_rmsd: bool = False,
+        rmsd_threshold: float = 1.0,
     ) -> IngestionBatchResult:
         result = IngestionBatchResult()
 
@@ -520,6 +527,8 @@ class IngestionService:
                 inchikey=inchikey,
                 smiles=smiles,
                 reference=reference,
+                check_rmsd=check_rmsd,
+                rmsd_threshold=rmsd_threshold,
             )
             if pose_created:
                 result.poses_created += 1
@@ -800,6 +809,9 @@ class IngestionService:
 
         elif scaffold_route:
             ### SUPPLEMENT THE SCAFFOLD ROWS FROM KNOWN ROUTE
+
+            mrich.var('DEBUG reaction_ids in ComponentModel', list(ComponentModel.objects.filter(route=scaffold_route._id, component_type=1).values_list('component_ref', flat=True)))
+            mrich.var('DEBUG ReactionModel PKs', list(ReactionModel.objects.values_list('pk', flat=True)))
 
             assert scaffold_route.num_reactions == 1
 
